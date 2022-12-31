01ff252f8599f41ee2a5b0f3f7c18548ade3987a
Patrick Lafrance <patrick.lafrance@gmail.com>
2022-12-31 15:10:11 -0500

    Ajouté un retour négatif en cas d'erreur

--- exercises/config-user/files/.verification.sh
+++ exercises/config-user/files/.verification.sh
@@ -2,11 +2,11 @@
 
 name=$(git config --get --local user.name)
 prevname=$(git config --get --local user.prevname)
-[ "$name" == "" ] || echo "Pas de nom configuré"
-[ "$name" == "$prevname" ] || echo "Le nom devrait être $prevname"
+[ "$name" == "" ] || echo "Pas de nom configuré" && exit 1
+[ "$name" == "$prevname" ] || echo "Le nom devrait être $prevname" && exit 1
 
 email=$(git config --get --local user.email)
 prevemail=$(git config --get --local user.prevemail)
-[ "$email" == "" ] || echo "Pas de courriel configuré"
-[ "$email" == "$prevemail" ] || echo "Le courriel devrait être $prevemail"
+[ "$email" == "" ] || echo "Pas de courriel configuré" && exit 1
+[ "$email" == "$prevemail" ] || echo "Le courriel devrait être $prevemail" && exit 1
 
