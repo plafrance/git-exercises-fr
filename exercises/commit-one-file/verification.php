ecaa82b97a29c2996cc9c57c2c65dd372d957189
Patrick Lafrance <patrick.lafrance@gmail.com>
2022-12-31 18:19:15 -0500

    Ajouté la vérification du message

--- exercises/commit-one-file/verification.php
+++ exercises/commit-one-file/verification.php
@@ -3,6 +3,7 @@ namespace GitExercises\hook\verifications;
 
 use GitExercises\hook\AbstractVerification;
 use GitExercises\hook\utils\ConsoleUtils;
+use GitExercises\hook\utils\GitUtils;
 
 class CommitOneFile extends AbstractVerification
 {
@@ -10,6 +11,8 @@ class CommitOneFile extends AbstractVerification
     {
         $commit = $this->ensureCommitsCount(1);
         $file = $this->ensureFilesCount($commit, 1);
-        $this->ensure(in_array($file, ['A.txt', 'B.txt']), 'None of the generated files has been commited. Received %s.', [ConsoleUtils::blue($file)]);
+        $this->ensure($file == 'B.txt', 'The wrong file has been commited. Expected B.txt bet received %s.', [ConsoleUtils::blue($file)]);
+        $message = GitUtils::getCommitSubject($commit);
+        $this->ensure($message == "Ajout du fichier B.txt", "Mauvais message. Attendu : «Ajout du fichier B.txt», reçu : «%s»", [ConsoleUtils::blue($message)]);
     }
 }
