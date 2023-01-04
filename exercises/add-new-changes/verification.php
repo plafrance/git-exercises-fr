b74eb5d20ef172a8d06544914c37ab8831b8861c
Patrick Lafrance <patrick.lafrance@gmail.com>
2023-01-04 18:54:29 -0500

    Simplifié

--- exercises/add-new-changes/verification.php
+++ exercises/add-new-changes/verification.php
@@ -16,8 +16,8 @@ class AddNewChanges extends AbstractVerification
         $message = GitUtils::getCommitSubject($commit);
         $this->ensure($message == "Traduction du message de bienvenue", "Mauvais message. Attendu : «Traduction du message de bienvenue», reçu : «%s»", [ConsoleUtils::blue($message)]);
 
-        $contenu_attendu = '#include <iostream>\n\nint main(int argc, char *argv[])\n{\n  cout << "Bonjour tout le monde!";\n  return 0;\n}\n';
-        $contents = implode($this->getFileContent($commit, "helloworld.cpp"));
+        $contenu_attendu = '  cout << "Bonjour tout le monde!';
+        $contents = $this->getFileContent($commit, "helloworld.cpp")[4];
         $this->ensure($contents == $contenu_attendu, "Le fichier ne contient pas les derniers changements\nAttendu: %s\nReçu: %s", [$contenu_attendu, $contents]);
     }
 }
