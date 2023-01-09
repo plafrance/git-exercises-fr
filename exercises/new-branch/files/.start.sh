f9a2006cbe516054967db9313a30f61fc94df595
Patrick Lafrance <patrick.lafrance@gmail.com>
2023-01-09 01:19:59 -0500

    Supprimé tout affichage de .start.sh

--- exercises/new-branch/files/.start.sh
+++ exercises/new-branch/files/.start.sh
@@ -1,6 +1,8 @@
-git branch -D old-branch &>/dev/null
-git checkout -b old-branch
-touch new_file
-git add new_file
-git commit -m "new_file"
-git branch -D new-branch
+#!/usr/bin/env bash
+
+( git branch -D old-branch  && \
+git checkout -b old-branch && \
+touch new_file && \
+git add new_file && \
+git commit -m "new_file" && \
+git branch -D new-branch ) &>/dev/null
