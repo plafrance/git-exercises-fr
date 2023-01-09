9c915687937fad55d5c7331cb14ca239674711ef
Patrick Lafrance <patrick.lafrance@gmail.com>
2023-01-09 17:14:40 -0500

    Changé la ligne en conflit pour du code

--- exercises/merge-conflict/verification.php
+++ exercises/merge-conflict/verification.php
@@ -12,8 +12,8 @@ class MergeConflict extends AbstractVerification
         $parents = GitUtils::getParents($mergeCommit);
         $this->ensure(count($parents) == 2, 'The last commit should be a merge commit.');
         $lines = $this->getFileContent($mergeCommit, 'equation.txt');
-        $this->ensure(count($lines) >= 1, 'You didn\'t resolve the conflict properly (too few lines).');
+        $this->ensure(count($lines) == 1, 'You didn\'t resolve the conflict properly (there must be only one line).');
         $equation = preg_replace('#\s+#', '', $lines[0]);
-        $this->ensure($equation == '2+3=5' || $equation == '3+2=5', 'You didn\'t resolve the conflict properly (expected 2+3=5 equation).');
+        $this->ensure($equation == 'print("2+3=5")' || $equation == 'print("3+2=5")', 'You didn\'t resolve the conflict properly (expected 2+3=5 equation).');
     }
 }
