81b791a19412d9081cb698ebe3710b7ae22e4647
Patrick Lafrance <patrick.lafrance@gmail.com>
2023-01-09 01:30:05 -0500

    Corrigé un cas d'erreur

--- exercises/new-branch/files/.start.sh
+++ exercises/new-branch/files/.start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-( git branch -D old-branch  && \
-git checkout -b old-branch && \
-touch new_file && \
-git add new_file && \
-git commit -m "new_file" && \
-git branch -D new-branch ) &>/dev/null
+(git checkout -b old-branch; \
+ touch new_file && \
+ git add new_file && \
+ git commit -m "new_file" && \
+ git branch -D new-branch ) &>/dev/null
