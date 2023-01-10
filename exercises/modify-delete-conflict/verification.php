5c906ad525155a4d74027c304be6bff2410ae455
Patrick Lafrance <patrick.lafrance@gmail.com>
2023-01-09 23:50:22 -0500

    Ajout de la vérification

---
+++ exercises/modify-delete-conflict/verification.php
@@ -0,0 +1,24 @@
+<?php
+namespace GitExercises\hook\verifications;
+
+use GitExercises\hook\AbstractVerification;
+use GitExercises\hook\utils\ConsoleUtils;
+use GitExercises\hook\utils\GitUtils;
+
+class ModifyDeleteConflict extends AbstractVerification
+{
+    protected function doVerify()
+    {
+        $mergeCommit = $this->ensureCommitsCount(4)[0];
+        $parents = GitUtils::getParents($mergeCommit);
+        $this->ensure(count($parents) == 2, 'The last commit should be a merge commit.');
+
+        $file = $this->ensureFilesCount($commit, 2);
+
+        $files = GitUtils::getChangedFiles($commit);
+        $this->ensure(isset($files['supprimer.txt']), "You were supposed to remove ignored.txt file.");
+        $this->ensure($files['supprimer.txt'] == 'D', "You were supposed to remove ignored.txt file, not change it.");
+        $this->ensure(isset($files['conserver.txt']), "You were supposed to modify ignored.txt file.");
+        $this->ensure($files['conserver.txt'] == 'M', "You were supposed to modify ignored.txt file, not delete it.");
+    }
+}
